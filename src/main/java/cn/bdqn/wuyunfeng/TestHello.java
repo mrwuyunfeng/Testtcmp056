d1d6aea3b59d98a2245a35a0489dae3878f8284f
mrwuyunfeng <913821317@qq.com>
2017-08-03 17:42:41 +0800

    sd

---
+++ src/main/java/cn/bdqn/wuyunfeng/TestHello.java
@@ -0,0 +1,11 @@
+package cn.bdqn.wuyunfeng;
+
+import org.junit.Test;
+
+
+public class TestHello {
+    @Test
+    public void testSayHello() {
+        System.out.println("hello world");
+    }
+}
